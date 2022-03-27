11b50cd58f2b5e9c644180b756fa20b1ee4cb268
abdul-musawwir007 <abdul.musawwir@price-factor.com>
2022-03-28 01:20:45 +0500

    added navbar and some other changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,29 +1,166 @@
-import React from "react";
-import { TextField } from "@mui/material"; 
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
 import styles from './navbar.module.css';
 import PersonIcon from '@mui/icons-material/Person';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
-export default function Navbar(props){
-    return (
-        <nav className={styles.nav}>
-            <div className={styles.navItem}>
-                <h2 className={styles.title}>AI Chapters</h2>
-            </div>
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-            <div className={styles.navItem} style={{flex:2}}>
-            <TextField
-            size="small"
-            style={{width:"100%"}}
-            label="Search input"
-            InputProps={{
-              type: 'search'
-            }}
-            />
-            </div>
+export default function Navbar(props) {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-            <div className={styles.navItem}>
-                <PersonIcon></PersonIcon>
-            </div>
-        </nav>
-    )
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              AI Chapters
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              AI Chapters
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            {/* <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box> */}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </nav>
+  );
+  // return (
+  //     <nav className={styles.nav}>
+  //         <div className={styles.navItem}>
+  //             <h2 className={styles.title}>AI Chapters</h2>
+  //         </div>
+
+  //         {/* <div className={styles.navItem} style={{flex:2}}>
+  //         <TextField
+  //         size="small"
+  //         style={{width:"100%"}}
+  //         label="Search input"
+  //         InputProps={{
+  //           type: 'search'
+  //         }}
+  //         />
+  //         </div>
+
+  //         <div className={styles.navItem}>
+  //             <PersonIcon></PersonIcon>
+  //         </div> */}
+  //     </nav>
+  // )
 }
